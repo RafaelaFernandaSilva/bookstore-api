a720526d4922e93ca890879b36eeb76bfb10bccb
RafaelaFernandaSilva <rafaelafernandasilveira@gmail.com>
2021-05-08 14:44:05 -0300

    Repositories implementados

---
+++ src/main/java/com/rafaela/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,12 @@
+package com.rafaela.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rafaela.bookstore.domain.Livro;
+
+@Repository 
+public interface LivroRepository extends JpaRepository<Livro, Integer > {
+	
+
+}
